bfa88edca2454c16dbe41299c65e59ff95809437
avalldeperas <albertvalldeperas@gmail.com>
2020-06-15 12:13:49 +0200

    init

---
+++ src/main/java/com/avalldeperas/awsimageupload/AwsImageUploadApplication.java
@@ -0,0 +1,18 @@
+package com.avalldeperas.awsimageupload;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsImageUploadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsImageUploadApplication.class, args);
+
+
+
+	}
+
+
+
+}
---
+++ src/main/java/com/avalldeperas/awsimageupload/config/AWSConfig.java
@@ -0,0 +1,8 @@
+package com.avalldeperas.awsimageupload.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AWSConfig {
+
+}
