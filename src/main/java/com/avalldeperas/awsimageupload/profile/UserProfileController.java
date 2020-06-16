5b1ef47df7f2cb23a62b337eac477679444562b0
avalldeperas <albertvalldeperas@gmail.com>
2020-06-16 14:10:15 +0200

    implements photo upload functionality in controller and service

--- src/main/java/com/avalldeperas/awsimageupload/profile/UserProfileController.java
+++ src/main/java/com/avalldeperas/awsimageupload/profile/UserProfileController.java
@@ -1,16 +1,20 @@
 package com.avalldeperas.awsimageupload.profile;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("api/v1/user-profile")
 public class UserProfileController {
 
+    private static final Logger logger = LogManager.getLogger(UserProfileController.class);
     private final UserProfileService userProfileService;
 
     @Autowired
@@ -20,7 +24,18 @@ public class UserProfileController {
 
     @GetMapping
     public List<UserProfile> getUserProfiles(){
+        logger.info("inside getUserProfiles()");
         return userProfileService.getUserProfiles();
+    }
+
+    @PostMapping(
+            path = "{userProfileId}/image/upload",
+            consumes = MediaType.MULTIPART_FORM_DATA_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public void uploadUserProfileImage(@PathVariable UUID userProfileId, @RequestParam MultipartFile file){
+        logger.info("inside uploadUserProfileImage() - userProfileId = " + userProfileId);
+        userProfileService.uploadUserProfileImage(userProfileId, file);
     }
 
 }
--- src/main/java/com/avalldeperas/awsimageupload/profile/UserProfileService.java
+++ src/main/java/com/avalldeperas/awsimageupload/profile/UserProfileService.java
@@ -1,21 +1,69 @@
 package com.avalldeperas.awsimageupload.profile;
 
+import com.amazonaws.services.kendra.model.ContentType;
+import com.avalldeperas.awsimageupload.buckets.BucketName;
+import com.avalldeperas.awsimageupload.filestore.FileStore;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
-import java.util.List;
+import java.io.IOException;
+import java.util.*;
+
+import static org.apache.http.entity.ContentType.*;
 
 @Service
 public class UserProfileService {
 
     private final UserProfileDataAccessService userProfileDataAccessService;
+    private final FileStore fileStore;
 
     @Autowired
-    public UserProfileService(UserProfileDataAccessService userProfileDataAccessService) {
+    public UserProfileService(UserProfileDataAccessService userProfileDataAccessService, FileStore fileStore) {
         this.userProfileDataAccessService = userProfileDataAccessService;
+        this.fileStore = fileStore;
     }
 
-    public List<UserProfile> getUserProfiles() {
+    List<UserProfile> getUserProfiles() {
         return userProfileDataAccessService.getUserProfiles();
+    }
+
+    void uploadUserProfileImage(UUID userProfileId, MultipartFile file) {
+        ifFileEmpty(file);
+        isFileImage(file);
+        UserProfile user = getUserProfileOrThrow(userProfileId);
+        Map<String, String> metadata = extractMetadata(file);
+
+        String path = String.format("%s/%s", BucketName.PROFILE_IMAGE.getBucketName(), user.getUserProfileId());
+        String fileName = String.format("%s-%s", file.getName(), user.getUserProfileId());
+        try {
+            fileStore.save(path, fileName, file.getInputStream(), Optional.of(metadata));
+        } catch (IOException e) {
+            throw new IllegalStateException(e);
+        }
+    }
+
+    private Map<String, String> extractMetadata(MultipartFile file) {
+        Map<String, String> metadata = new HashMap<>();
+        metadata.put("Content-Type", file.getContentType());
+        metadata.put("Content-Length", String.valueOf(file.getSize()));
+        return metadata;
+    }
+
+    private UserProfile getUserProfileOrThrow(UUID userProfileId) {
+        return userProfileDataAccessService.getUserProfiles().stream()
+                    .filter(userProfile -> userProfile.getUserProfileId().equals(userProfileId))
+                    .findFirst()
+                    .orElseThrow(() -> new IllegalStateException(String.format("User profile %s not found", userProfileId)));
+    }
+
+    private void isFileImage(MultipartFile file) {
+        if(!Arrays.asList(IMAGE_JPEG, IMAGE_GIF, IMAGE_PNG).contains(file.getContentType()))
+            throw new IllegalStateException("File must be an image.");
+    }
+
+    private void ifFileEmpty(MultipartFile file) {
+        if(file.isEmpty())
+            throw new IllegalStateException("Cannot upload empty file ["+ file.getSize() + "]");
     }
 }
